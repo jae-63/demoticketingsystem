13dc2ddf4603d088b5187f42ed8cbdcbaea6110f
Jonathan Epstein <JonathanEpstein9@gmail.com>
2018-06-01 18:49:44 -0400

    Add files via upload
    fix unit tests

--- complete/src/test/java/ticketservice/SeatHoldTest.java
+++ complete/src/test/java/ticketservice/SeatHoldTest.java
@@ -1,7 +1,10 @@
 package test.tickserv; 
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test; 
-import org.junit.Before; 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.After;
 
 import tickserv.TicketService;
@@ -21,26 +24,55 @@ public class SeatHoldTest {
     private static SeatHold testSeatHold2;
     private static SeatHold testSeatHold3;
     private static SeatHold testSeatHold4;
+    private static SeatHold testSeatHold5;
+    private static SeatHold testSeatHold6;
+    private static SeatHold testSeatHold7;
+    private static SeatHold testSeatHold8;
+    private static SeatHold testSeatHold9;
+    private static SeatHold testSeatHold10;
+
+
+
+
 
     /* use a relatively small auditorium for these tests, for simplicity and readability */
     private static final int AUD_DEPTH = 6;
-    private static final int AUD_WIDTH = 7;
+    private static final int AUD_WIDTH = 8;
     private static final int HOLD_EXP_IN_SECONDS = 2;
     private static final String customerEmail = "test@example.com";
+    private static final String alternateCustomerEmail = "test2@bar.com";
 
-@Before
-public void before() throws Exception {
+@BeforeClass
+public static void beforeClass() throws Exception {
     testTicketService = new TicketService(AUD_DEPTH, AUD_WIDTH, HOLD_EXP_IN_SECONDS);
 
     testSeatHold1 = new SeatHold(5, customerEmail, testTicketService);
-    testSeatHold2 = new SeatHold(4, customerEmail, testTicketService);
+    testSeatHold2 = new SeatHold(4, alternateCustomerEmail, testTicketService);
 
     /* by the time we get here the good seats should already be taken */
     testSeatHold3 = new SeatHold(6, customerEmail, testTicketService);
 
     /* this request should fail entirely since we can't fit 9 customers into a 6-seat row */
     testSeatHold4 = new SeatHold(9, customerEmail, testTicketService);
+
+    testSeatHold5 = new SeatHold(6, customerEmail, testTicketService);
+    testSeatHold6 = new SeatHold(6, customerEmail, testTicketService);
+    testSeatHold7 = new SeatHold(6, customerEmail, testTicketService);
+
+    /* by the time we get here the house should be too full for a wide order like this */
+    testSeatHold8 = new SeatHold(5, customerEmail, testTicketService);
+
+    /* but with plenty of room for a narrow order like this */
+    testSeatHold9 = new SeatHold(2, customerEmail, testTicketService);
+
+    /* wait until these held-but-not-reserved orders expire */
+    TimeUnit.SECONDS.sleep(HOLD_EXP_IN_SECONDS+1);
+
+    /* now there should be plenty of room for this order, since time has elapsed*/
+    testSeatHold10 = new SeatHold(5, customerEmail, testTicketService);
+
 }
+
 
 @After
 public void after() throws Exception { 
@@ -72,9 +104,14 @@ public void testGetSeatsRow() throws Exception {
     assert(testSeatHold1.getSeatsRow() >= 1);
     assert(testSeatHold1.getSeatsRow() <= 2); /* not back in the cheap seats */
 
-/*    assert(testSeatHold3.getSeatsRow() >= 1);
+    assert(testSeatHold3.getSeatsRow() >= 1);
     assert(testSeatHold3.getSeatsRow() > 2); /* back in the cheap seats! */
-} 
+
+
+
+    assert(testSeatHold7.getSeatsRow() == 6);
+
+}
 
 /** 
 * 
@@ -83,8 +120,9 @@ public void testGetSeatsRow() throws Exception {
 */ 
 @Test
 public void testGetSeatsLeftmost() throws Exception { 
-//TODO: Test goes here... 
-} 
+    assert(testSeatHold1.getSeatsLeftmost() >= 2); /* not on the aisle since this is the front row */
+    assert(testSeatHold3.getSeatsLeftmost() == 1);  /* further back in the auditorium so therefore on the aisle */
+}
 
 /** 
 * 
@@ -93,8 +131,9 @@ public void testGetSeatsLeftmost() throws Exception {
 */ 
 @Test
 public void testGetNumSeats() throws Exception { 
-//TODO: Test goes here... 
-} 
+    assert(testSeatHold1.getNumSeats() == 5);
+    assert(testSeatHold5.getNumSeats() == 6);
+}
 
 /** 
 * 
@@ -102,19 +141,11 @@ public void testGetNumSeats() throws Exception {
 * 
 */ 
 @Test
-public void testGetCustomerEmail() throws Exception { 
-//TODO: Test goes here... 
+public void testGetCustomerEmail() throws Exception {
+    assert(testSeatHold1.getCustomerEmail().equals(customerEmail));
+    assert(testSeatHold2.getCustomerEmail().equals(alternateCustomerEmail));
 } 
 
-/** 
-* 
-* Method: getTimeHeld() 
-* 
-*/ 
-@Test
-public void testGetTimeHeld() throws Exception { 
-//TODO: Test goes here... 
-} 
 
 /** 
 * 
@@ -122,39 +153,20 @@ public void testGetTimeHeld() throws Exception {
 * 
 */ 
 @Test
-public void testGetIsValid() throws Exception { 
-//TODO: Test goes here... 
-} 
+public void testGetIsValid() throws Exception {
+    assert(! testSeatHold4.getIsValid()); /* couldn't fill this extra-wide request */
 
-/** 
-* 
-* Method: scoreSeatQuality(int row, int seatNumberInRow, int auditoriumDepth, int auditoriumWidth) 
-* 
-*/ 
-@Test
-public void testScoreSeatQuality() throws Exception { 
-//TODO: Test goes here... 
-} 
+    assert(testSeatHold5.getIsValid());
+    assert(testSeatHold6.getIsValid());
+    assert(testSeatHold7.getIsValid()); /* these are the last seats in the back of the hall */
+
+    assert(! testSeatHold8.getIsValid()); /* the hall couldn't accommodate such a large order */
+    assert(testSeatHold9.getIsValid()); /* but there was room for a small order */
+
+    assert(testSeatHold10.getIsValid()); /* after some time passed, an order which failed above could now be fulfilled */
+
+}
 
 
-/** 
-* 
-* Method: findBlockOfSeats(int numSeats, tickserv.TicketService ticketService) 
-* 
-*/ 
-@Test
-public void testFindBlockOfSeats() throws Exception { 
-//TODO: Test goes here... 
-/* 
-try { 
-   Method method = SeatHold.getClass().getMethod("findBlockOfSeats", int.class, tickserv.TicketService.class); 
-   method.setAccessible(true); 
-   method.invoke(<Object>, <Parameters>); 
-} catch(NoSuchMethodException e) { 
-} catch(IllegalAccessException e) { 
-} catch(InvocationTargetException e) { 
-} 
-*/ 
-} 
 
 } 
--- complete/src/test/java/ticketservice/TicketServiceTest.java
+++ complete/src/test/java/ticketservice/TicketServiceTest.java
@@ -1,7 +1,8 @@
 package test.tickserv; 
 
 import org.junit.Test; 
-import org.junit.Before; 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.After;
 
 import tickserv.TicketService;
@@ -16,14 +17,31 @@ import tickserv.TicketService;
 */ 
 public class TicketServiceTest {
     private static TicketService testTicketService;
+    private static TicketService virginTicketService; /* another service which hasn't been modified yet */
+
+    private static tickserv.SeatHold testSeatHold1;
+    private static tickserv.SeatHold testSeatHold2;
+    private static tickserv.SeatHold testSeatHold3;
+    private static tickserv.SeatHold testSeatHold4;
+    private static tickserv.SeatHold testSeatHold5;
+    private static tickserv.SeatHold testSeatHold6;
+
+    private static final String customerEmail = "test@example.com";
+    private static final String alternateCustomerEmail = "test2@bar.com";
+
     /* use a relatively small auditorium for these tests, for simplicity and readability */
     private static final int AUD_DEPTH = 6;
-    private static final int AUD_WIDTH = 7;
-    private static final int HOLD_EXP_IN_SECONDS = 2;
+    private static final int AUD_WIDTH = 8;
+    private static final int HOLD_EXP_IN_SECONDS = 200;
 
-@Before
-public void before() throws Exception {
+@BeforeClass
+public static void beforeClass() throws Exception {
     testTicketService = new TicketService(AUD_DEPTH, AUD_WIDTH, HOLD_EXP_IN_SECONDS);
+    virginTicketService = new TicketService(AUD_DEPTH, AUD_WIDTH, HOLD_EXP_IN_SECONDS);
+
+    testSeatHold1 = testTicketService.findAndHoldSeats(5, customerEmail);
+    testSeatHold2 = testTicketService.findAndHoldSeats(4, customerEmail);
+
 } 
 
 @After
@@ -49,25 +67,6 @@ public void testGetAuditoriumDepth() throws Exception {
 public void testGetAuditoriumWidth() throws Exception {
     assert (testTicketService.getAuditoriumWidth() == AUD_WIDTH);
 }
-    /**
-* 
-* Method: revertExpiredHolds() 
-* 
-*/ 
-@Test
-public void testRevertExpiredHolds() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: setAuditoriumSeatsStatus(int seatsRow, int seatNum, SeatStatus newStatus) 
-* 
-*/ 
-@Test
-public void testSetAuditoriumSeatsStatus() throws Exception { 
-//TODO: Test goes here... 
-} 
 
 /** 
 * 
@@ -76,58 +75,11 @@ public void testSetAuditoriumSeatsStatus() throws Exception {
 */ 
 @Test
 public void testGetAuditoriumSeatsStatus() throws Exception { 
-//TODO: Test goes here... 
+//TODO: Test goes here...
+    assert(virginTicketService.getAuditoriumSeatsStatus(1,1) == TicketService.SeatStatus.AVAILABLE);
+    assert(testTicketService.getAuditoriumSeatsStatus(1,3) != TicketService.SeatStatus.AVAILABLE);
 } 
 
-/** 
-* 
-* Method: setAuditoriumHoldId(int seatsRow, int seatNum, int holdId) 
-* 
-*/ 
-@Test
-public void testSetAuditoriumHoldId() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: setAuditoriumHoldTime(int seatsRow, int seatNum, long timeHeld) 
-* 
-*/ 
-@Test
-public void testSetAuditoriumHoldTime() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: getAuditoriumHoldTime(int seatsRow, int seatNum) 
-* 
-*/ 
-@Test
-public void testGetAuditoriumHoldTime() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: setAuditoriumReservedSeatEmails(int seatsRow, int seatNum, String email) 
-* 
-*/ 
-@Test
-public void testSetAuditoriumReservedSeatEmails() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: getAuditoriumReservedSeatEmails(int seatsRow, int seatNum) 
-* 
-*/ 
-@Test
-public void testGetAuditoriumReservedSeatEmails() throws Exception { 
-//TODO: Test goes here... 
-} 
 
 /** 
 * 
@@ -136,7 +88,7 @@ public void testGetAuditoriumReservedSeatEmails() throws Exception {
 */ 
 @Test
 public void testShowAuditoriumMap() throws Exception {
-    String [] map = testTicketService.showAuditoriumMap(0);
+    String [] map = virginTicketService.showAuditoriumMap(0);
 
     assert(map.length == AUD_DEPTH);
     assert(map[0].length() == AUD_WIDTH);
@@ -149,7 +101,7 @@ public void testShowAuditoriumMap() throws Exception {
 */ 
 @Test
 public void testNumSeatsAvailable() throws Exception { 
-    int countAvailable = testTicketService.numSeatsAvailable();
+    int countAvailable = virginTicketService.numSeatsAvailable();
 
     assert(countAvailable == AUD_DEPTH * AUD_WIDTH);
 }
@@ -161,7 +113,9 @@ public void testNumSeatsAvailable() throws Exception {
 */ 
 @Test
 public void testFindAndHoldSeats() throws Exception { 
-//TODO: Test goes here... 
+    testSeatHold3 = testTicketService.findAndHoldSeats(4, customerEmail);
+
+//    assert(testSeatHold3.getSeatHoldIndex() == 3);
 } 
 
 /** 
@@ -171,7 +125,8 @@ public void testFindAndHoldSeats() throws Exception {
 */ 
 @Test
 public void testReserveSeats() throws Exception { 
-//TODO: Test goes here... 
+    String reserve1 = testTicketService.reserveSeats(testSeatHold1.getSeatHoldIndex(),customerEmail);
+    assert(! reserve1.startsWith("FAILURE"));
 } 
 
 
